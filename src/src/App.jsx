210e738a2dd5fdad5738dd200c96cbbe2a9f1c51
Peeyush2003 <peeyushshandilya76@gmail.com>
2026-01-29 16:34:50 +0530

    Add initial App component for Academic Program Planner

---
+++ src/src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>Academic Program Planner</h1>
+      <p>Frontend deployed successfully 🎉</p>
+    </div>
+  );
+}
+
+export default App;
